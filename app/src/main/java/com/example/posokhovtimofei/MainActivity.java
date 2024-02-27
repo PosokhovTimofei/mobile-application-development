121df93385d98bc2f95ed21744887a37189b2e64
Posokhov Timofei <new-ur@list.ru>
2024-02-27 16:40:46 +0300

    second practice

--- app/src/main/java/com/example/posokhovtimofei/MainActivity.java
+++ app/src/main/java/com/example/posokhovtimofei/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,15 +14,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button  = findViewById(R.id.button);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(intent);
-            }
-        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("KKK", "START");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("KEY", "RESUME");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("KEY", "PAUSE");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("KEY", "RESTART");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("KEY", "STOP");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("KKK", "DESTROY");
     }
 }
 
